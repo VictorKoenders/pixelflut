dc85f7298d43146e2e3648779040b2e1d80094e6
Victor Koenders <victor.koenders@gmail.com>
2018-09-12 10:12:20 +0200

    Added test for max_threads networking and fixed issues

--- src/handlers/max_threads.rs
+++ src/handlers/max_threads.rs
@@ -33,30 +33,84 @@ fn render_loop(screen: &Screen) {
     }
 }
 
+struct StreamReader {
+    pub buffer: [u8; 100],
+    pub start_index: usize,
+}
+
+impl Default for StreamReader {
+    fn default() -> StreamReader {
+        StreamReader {
+            buffer: [0u8; 100],
+            start_index: 0,
+        }
+    }
+}
+
+impl StreamReader {
+    pub fn parse_contents(&mut self, bytes_read: usize, mut callback: impl FnMut(&[u8])) {
+        let total_len = self.start_index + bytes_read;
+        let mut current_index = 0;
+
+        while let Some(index) = self.buffer[..total_len]
+            .iter()
+            .skip(current_index)
+            .position(|b| b == &b'\n')
+        {
+            callback(&self.buffer[current_index..current_index + index]);
+            current_index += index + 1;
+        }
+
+        if current_index > 0 {
+            unsafe {
+                ptr::copy(
+                    &self.buffer[current_index],
+                    &mut self.buffer[0],
+                    total_len - current_index,
+                );
+            }
+
+            self.start_index = total_len - current_index;
+        } else {
+            self.start_index = total_len - current_index;
+        }
+    }
+}
+
 fn run_client(mut socket: TcpStream) -> Result<()> {
-    let mut buffer = [0u8; 100];
-    let mut start_index = 0;
+    let mut reader = StreamReader::default();
     loop {
-        let len = socket.read(&mut buffer[start_index..])?;
+        let len = socket.read(&mut reader.buffer[reader.start_index..])?;
         if len == 0 {
             break;
         }
-        let total_len = start_index + len;
-
-        start_index = 0;
-
-        while let Some(index) = buffer.iter().skip(start_index).position(|b| b == &b'\n') {
-            let _ = Client.handle_message(&mut socket, &buffer[start_index..start_index + index]);
-            start_index += index;
-        }
-        unsafe {
-            ptr::copy(
-                &buffer[start_index],
-                &mut buffer[0],
-                total_len - start_index,
-            );
-        }
-        start_index = total_len - start_index;
+        reader.parse_contents(len, |buff| {
+            let _ = Client.handle_message(&mut socket, buff);
+        });
     }
     Ok(())
+}
+
+#[test]
+pub fn test_stream_reader() {
+    let message = b"PX 100 100 FFAABB\nPX 100 100 FFAABB\nPX 100 100 FFAABB\nPX 100 100 FFAABB\n";
+
+    for i in 1..100 {
+        let mut reader = StreamReader::default();
+        let mut j = 0;
+        'j_loop: while j < message.len() {
+            for x in 0..i {
+                let message_index = j + x;
+                let buffer_index = reader.start_index + x;
+                if message_index >= message.len() {
+                    break 'j_loop;
+                }
+                reader.buffer[buffer_index] = message[message_index];
+            }
+            reader.parse_contents(i, |buff| {
+                assert_eq!("PX 100 100 FFAABB", ::std::str::from_utf8(buff).unwrap());
+            });
+            j += i;
+        }
+    }
 }
