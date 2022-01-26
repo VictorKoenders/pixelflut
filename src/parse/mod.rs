2d9b9e9380e4fb6f0de9c10377d0bff44584a7d0
Victor Koenders <gpg@trangar.com>
2022-01-26 19:41:46 +0100

    Added initial AFL implementation

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -39,3 +39,34 @@ fn validate() {
     assert_eq!(std::parse_coordinate(str.as_bytes()), None);
     assert_eq!(bytewise::parse_coordinate(str.as_bytes()), None);
 }
+
+#[test]
+fn crashes() {
+    // crashes found with running the following commint in the ./fuzz/ folder:
+    // `cargo afl fuzz -i in -o out target/debug/pixelflut_fuzzer`
+    let cases = [
+        b" \\".as_slice(),
+        b" > \n".as_slice(),
+        b" ".as_slice(),
+        b"mmmmm ".as_slice(),
+    ];
+
+    #[cfg(feature = "memory-cache")]
+    let memcache = {
+        let mut cache = memcache::NumCache::new();
+        cache.init();
+        cache
+    };
+    for case in cases {
+        let result = bytewise::parse_coordinate(case);
+        assert_eq!(result, std::parse_coordinate(case));
+
+        #[cfg(feature = "memory-cache")]
+        match (result, memcache.parse_coordinate(case)) {
+            (Some(x), None) if x.0 > MAX_VALID_NUMBER => {}
+            (Some(x), Some(y)) if x == y => {}
+            (None, None) => {}
+            _ => panic!("bytewise and memcache did not agree on {:?}", data),
+        }
+    }
+}
